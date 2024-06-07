a0d8bd96b99f9721b619660cd825ef08ca8da4fa
Annmariya3820 <mariyadaisy320@gmail.com>
2024-06-07 15:11:39 +0530

    completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <div>
       <nav class="navbar navbar-expand-lg bg-warning">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">STUDENT APP</a>
+    <Link class="navbar-brand" to="#">STUDENT APP</Link>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -14,13 +15,13 @@ const Navbar = () => {
       
        
         <li class="nav-item">
-          <a class="nav-link" href="/">Add Student</a>
+          <Link class="nav-link" to="/">Add Student</Link>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="/delete">Delete Student</a>
+          <Link class="nav-link" to="/delete">Delete Student</Link>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="/view">View Student</a>
+          <Link class="nav-link" to="/view">View Student</Link>
         </li>
     
       </ul>
